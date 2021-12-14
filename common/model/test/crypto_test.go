fbfb04852adcffddc5a6e38c8ebe4a6f532efe54
tt-loveslife <1450413390@qq.com>
2021-12-14 14:20:51 +0800

    modify cache related api

--- common/model/test/crypto_test.go
+++ common/model/test/crypto_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 	curve "zecrey-crypto/ecc/ztwistededwards/tebn254"
 
@@ -58,4 +59,13 @@ func TestGetEncCollisionTotalCount(t *testing.T) {
 	count, err := ctx.Crypto.GetEncCollisionTotalCount()
 	assert.Nil(t, err)
 	assert.Equal(t, count, int64(200000))
+}
+
+func TestGerRandomCollision(t *testing.T) {
+	// Get Service Context
+	ctx := TestServiceContext(ConfigProvider("test.yaml"))
+	enc_data, err := ctx.Crypto.GerRandomCollision()
+	assert.Nil(t, err)
+	fmt.Println(enc_data)
+	assert.NotNil(t, enc_data)
 }
